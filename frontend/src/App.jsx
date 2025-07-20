8ceb065f765c1cad29b39efec75b682ae3815fc4
vaishnavi-kale-22 <vaishnavikale2215@gmail.com>
2025-07-20 12:28:28 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   }, [code, review]);
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review/", {
+    const response = await axios.post("https://smart-code-reviewer.onrender.com", {
       code,
     });
 
